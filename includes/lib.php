b64c95c0620369cb4a88a6463240f88c0f953e99
François Gannaz <francois.gannaz@silecs.info>
2025-05-28 11:25:39 +0200

    Sort 'Time by developer'

--- includes/lib.php
+++ includes/lib.php
@@ -6,43 +6,159 @@
 namespace timereporting;
 
 /**
+ * @return array<int, list{name: string, categories: array}>  E.g. $projects[$pid]['categories'][$cid]['time']
+ */
+function getProjectTimeByCategory($projectId, string $after, string $before): array
+{
+    $timerows = readCategoriesTime($projectId, $after, $before);
+    $projects = [];
+    foreach ($timerows as $row) {
+        $id = (int) $row['id'];
+        if (!isset($projects[$id])) {
+            $projects[$id] = [
+                'name' => $row['name'],
+                'categories' => [],
+            ];
+        }
+        $cid = (int) $row['category_id'];
+        if (!isset($projects[$id]['categories'][$cid])) {
+            if ($cid > 0) {
+                $name = $row['category_name'];
+            } else {
+                $name = "-";
+            }
+            $projects[$id]['categories'][$cid] = [
+                'name' => $name,
+                'notes' => (int) $row['used_notes'],
+                'tickets' => (int) $row['used_tickets'],
+                'time' => (int) $row['used_time'],
+            ];
+        }
+    }
+    return $projects;
+}
+
+/**
+ * @return array<int, list{name: string, users: array}>  E.g. $projects[$pid]['user'][$uid]['time']
+ */
+function getProjectTimeByUser($projectId, string $after, string $before): array
+{
+    $timerows = readUsersTime($projectId, $after, $before);
+    $projects = [];
+    foreach ($timerows as $row) {
+        $id = (int) $row['id'];
+        if (!isset($projects[$id])) {
+            $projects[$id] = [
+                'name' => $row['name'],
+                'users' => [],
+            ];
+        }
+        $uid = (int) $row['user_id'];
+        if (!isset($projects[$id]['users'][$uid])) {
+            if ($uid > 0) {
+                $name = $row['user_name'];
+            } else {
+                $name = "-";
+            }
+            $projects[$id]['users'][$uid] = [
+                'name' => $name,
+                'notes' => (int) $row['used_notes'],
+                'tickets' => (int) $row['used_tickets'],
+                'time' => (int) $row['used_time'],
+            ];
+        }
+    }
+    return $projects;
+}
+
+/**
  * Array of id, name, parent_id, timeused, tt.timecredit
  *
+ * @internal
  * @param int $projectId
  * @return IteratorAggregate
  */
 function readCategoriesTime($projectId, string $after, string $before)
+{
+    $ids = getIds($projectId);
+    $condition = getCondition($after, $before);
+    $sql = <<<EOSQL
+        SELECT
+            p.id, p.name,
+            c.id AS category_id, c.name AS category_name,
+            SUM(bn.time_tracking) AS used_time,
+            COUNT(DISTINCT b.id) AS used_tickets,
+            COUNT(DISTINCT bn.id) AS used_notes
+        FROM {project} p
+            LEFT JOIN {category} c ON p.id = c.project_id
+            LEFT JOIN {bug} b ON b.project_id = p.id AND b.category_id = c.id
+            LEFT JOIN {bugnote} bn ON bn.bug_id = b.id
+        WHERE p.id IN ($ids) $condition
+        GROUP BY p.id, c.id
+        ORDER BY p.name, c.name
+        EOSQL;
+    return db_query($sql);
+}
+
+/**
+ * Array of id, name, parent_id, timeused, tt.timecredit
+ *
+ * @internal
+ * @param int $projectId
+ * @return IteratorAggregate
+ */
+function readUsersTime($projectId, string $after, string $before)
+{
+    $ids = getIds($projectId);
+    $condition = getCondition($after, $before);
+    $sql = <<<EOSQL
+        SELECT
+            p.id, p.name,
+            u.id AS user_id, u.realname AS user_name,
+            SUM(bn.time_tracking) AS used_time,
+            COUNT(DISTINCT b.id) AS used_tickets,
+            COUNT(DISTINCT bn.id) AS used_notes
+        FROM {project} p
+            LEFT JOIN {bug} b ON b.project_id = p.id
+            LEFT JOIN {bugnote} bn ON bn.bug_id = b.id
+            LEFT JOIN {user} u ON bn.reporter_id = u.id
+        WHERE p.id IN ($ids) $condition
+        GROUP BY p.id, u.id
+        ORDER BY p.name, used_time DESC, used_notes DESC
+        EOSQL;
+    return db_query($sql);
+}
+
+/**
+ * @internal
+ */
+function getIds($projectId)
 {
     if (empty($projectId) || $projectId == ALL_PROJECTS) {
         $projects = current_user_get_accessible_projects();
     } else {
         $projects = [(int) $projectId];
     }
-    $ids = join(',', $projects);
+    return join(',', $projects);
+}
 
+/**
+ * @internal
+ */
+function getCondition(string $after, string $before)
+{
     if ($after && $before) {
         $start = \DateTimeImmutable::createFromFormat('Y-m-d H:i', "$after 00:00")->getTimestamp();
         $end = \DateTimeImmutable::createFromFormat('Y-m-d H:i', "$before 23:59")->getTimestamp();
-        $condition = "AND bn.date_submitted BETWEEN $start AND $end";
-    } elseif ($after) {
-        $start = \DateTimeImmutable::createFromFormat('Y-m-d H:i', "$after 00:00")->getTimestamp();
-        $condition = "AND bn.date_submitted > $start";
-    } elseif ($before) {
-        $end = \DateTimeImmutable::createFromFormat('Y-m-d H:i', "$before 23:59")->getTimestamp();
-        $condition = "AND bn.date_submitted < $end";
-    } else {
-        $condition = "";
+        return "AND bn.date_submitted BETWEEN $start AND $end";
     }
-
-    $sql = "SELECT p.id, p.name, c.id AS category_id, c.name AS category_name,
-                SUM(bn.time_tracking) AS used_time, COUNT(DISTINCT b.id) AS used_tickets, COUNT(DISTINCT bn.id) AS used_notes
-            FROM {project} p
-            LEFT JOIN {category} c ON p.id = c.project_id
-            LEFT JOIN {bug} b ON b.project_id = p.id AND b.category_id = c.id
-            LEFT JOIN {bugnote} bn ON bn.bug_id = b.id
-            WHERE p.id IN ($ids) $condition
-            GROUP BY p.id, c.id
-            ORDER BY p.name, c.name";
-
-    return db_query($sql);
+    if ($after) {
+        $start = \DateTimeImmutable::createFromFormat('Y-m-d H:i', "$after 00:00")->getTimestamp();
+        return "AND bn.date_submitted > $start";
+    }
+    if ($before) {
+        $end = \DateTimeImmutable::createFromFormat('Y-m-d H:i', "$before 23:59")->getTimestamp();
+        return "AND bn.date_submitted < $end";
+    }
+    return "";
 }
--- pages/report.php
+++ pages/report.php
@@ -16,32 +16,6 @@ $before = $_GET['before'] ?? $_GET['end'] ?? '';
 if (!preg_match('/^\d{4}-\d\d-\d\d$/', $before)) {
 	$before = '';
 }
-$timerows = \timereporting\readCategoriesTime($projectId, $after, $before);
-
-$projects = [];
-foreach ($timerows as $row) {
-    $id = (int) $row['id'];
-    if (!isset($projects[$id])) {
-        $projects[$id] = [
-            'name' => $row['name'],
-            'categories' => [],
-        ];
-    }
-    $cid = (int) $row['category_id'];
-    if (!isset($projects[$id]['categories'][$cid])) {
-        if ($cid > 0) {
-            $name = $row['category_name'];
-        } else {
-            $name = "-";
-        }
-        $projects[$id]['categories'][$cid] = [
-            'name' => $name,
-            'notes' => (int) $row['used_notes'],
-            'tickets' => (int) $row['used_tickets'],
-            'time' => (int) $row['used_time'],
-        ];
-    }
-}
 
 layout_page_header("Rapport d'utilisation du temps");
 layout_page_begin();
@@ -57,7 +31,8 @@ print_summary_menu('TimeReporting/report', $filter);
 <div id="time-per-project" class="summary-container">
     <h2>Temps par catégories</h2>
     <?php
-    foreach ($projects as $pid => $project) {
+    $projectsCategories = \timereporting\getProjectTimeByCategory($projectId, $after, $before);
+    foreach ($projectsCategories as $pid => $project) {
         echo "<h3>" . htmlspecialchars($project['name']) . "</h3>\n";
         $total = [0, 0, 0];
         ?>
@@ -99,26 +74,75 @@ print_summary_menu('TimeReporting/report', $filter);
         Les durées sont exprimées sous la forme <em>hh:mm</em> (heures et minutes).
     </p>
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            Limiter ce tableau à une période (limites incluses)
-        </div>
-        <div class="panel-body">
-            <form method="GET" class="form-horizontal">
-                <input type="hidden" name="page" value="TimeReporting/report" />
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="after">À partir de</label>
-                    <div class="col-sm-10"><input type="date" name="after" value="<?= $after ?>" class="form-control" /></div>
-                </div>
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="before">Jusqu'à</label>
-                    <div class="col-sm-10"><input type="date" name="before" value="<?= $before ?>" class="form-control" /></div>
-                </div>
-                <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10"><button type="submit" class="btn btn-primary">Afficher</button></div>
-                </div>
-            </form>
-        </div>
+</div>
+
+<div id="time-per-user" class="summary-container">
+    <h2>Temps par développeur</h2>
+    <?php
+    $projectsUsers = \timereporting\getProjectTimeByUser($projectId, $after, $before);
+    foreach ($projectsUsers as $pid => $project) {
+        echo "<h3>" . htmlspecialchars($project['name']) . "</h3>\n";
+        $total = [0, 0, 0];
+        ?>
+        <table class="table table-bordered table-condensed table-hover table-striped" style="max-width: 70em">
+            <thead>
+                <tr class="row-user">
+                    <th>Utilisateur</th>
+                    <th class="align-right">Temps consacré</th>
+                    <th class="align-right">#tickets</th>
+                    <th class="align-right">#notes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($project['users'] as $row) {
+                    $total[0] += $row['time'];
+                    $total[1] += $row['tickets'];
+                    $total[2] += $row['notes'];
+                    echo "<tr>";
+                    echo "<td>" . htmlspecialchars($row['name']) . "</td>";
+                    echo '<td class="column-num">' . db_minutes_to_hhmm($row['time']) . "</td>";
+                    echo '<td class="column-num">' . $row['tickets'] . "</td>";
+                    echo '<td class="column-num">' . $row['notes'] . "</td>";
+                    echo "</tr>\n";
+                }
+                ?>
+                <tr>
+                    <th>Total</th>
+                    <td class="column-num"><?= db_minutes_to_hhmm($total[0]) ?></td>
+                    <td class="column-num"><?= $total[1] ?></td>
+                    <td class="column-num"><?= $total[2] ?></td>
+                </tr>
+            </tbody>
+        </table>
+    <?php
+    }
+    ?>
+    <p>
+        Les durées sont exprimées sous la forme <em>hh:mm</em> (heures et minutes).
+    </p>
+
+</div>
+
+<div class="panel panel-default">
+    <div class="panel-heading">
+        Limiter ces tableaux à une période (limites incluses)
+    </div>
+    <div class="panel-body">
+        <form method="GET" class="form-horizontal">
+            <input type="hidden" name="page" value="TimeReporting/report" />
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="after">À partir de</label>
+                <div class="col-sm-10"><input type="date" name="after" value="<?= $after ?>" class="form-control" /></div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="before">Jusqu'à</label>
+                <div class="col-sm-10"><input type="date" name="before" value="<?= $before ?>" class="form-control" /></div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10"><button type="submit" class="btn btn-primary">Afficher</button></div>
+            </div>
+        </form>
     </div>
 </div>
 
